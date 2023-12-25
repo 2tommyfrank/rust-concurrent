54af35653565aef1aaf88285bd04639176f437d9
2tommyfrank <thomas_v_frank@brown.edu>
2023-12-24 21:57:57 -0500

    fix(listset): find_mut to appease borrow checker

--- src/listset.rs
+++ src/listset.rs
@@ -16,8 +16,20 @@ struct Node<T> {
 
 type Link<T> = Option<Box<Node<T>>>;
 
-impl<T: Hashable> Hashable for Node<T> {
-    fn hash(&self) -> u64 { self.item.hash() }
+impl<T> Node<T> {
+    fn insert(at: &mut Link<T>, item: T) {
+        let new_node = Node { item, next: at.take() };
+        *at = Some(Box::new(new_node));
+    }
+    fn remove(at: &mut Link<T>) -> Result<T, &'static str> {
+        match at.take() {
+            Some(node) => {
+                *at = node.next;
+                Ok(node.item)
+            },
+            None => Err("cannot remove from empty list"),
+        }
+    }
 }
 
 impl<T: Hashable> Node<T> {
@@ -30,13 +42,31 @@ impl<T: Hashable> Node<T> {
     }
     fn find_mut(from: &mut Link<T>, key: u64) -> (&mut Link<T>, bool) {
         match from {
+            Some(node) if node.hash() < key => {},
+            Some(node) if node.hash() == key => return (from, true),
+            _ => return (from, false),
+        }
+        match from {
             Some(node) if node.hash() < key => {
                 Self::find_mut(&mut node.next, key)
             },
-            Some(node) if node.hash() == key => (from, true),
-            _ => (from, false),
+            _ => panic!(),
         }
     }
+    // requires Polonius (NLL problem case #3)
+    // fn find_mut(from: &mut Link<T>, key: u64) -> (&mut Link<T>, bool) {
+    //     match from {
+    //         Some(node) if node.hash() < key => {
+    //             Self::find_mut(&mut node.next, key)
+    //         },
+    //         Some(node) if node.hash() == key => (from, true),
+    //         _ => (from, false),
+    //     }
+    // }
+}
+
+impl<T: Hashable> Hashable for Node<T> {
+    fn hash(&self) -> u64 { self.item.hash() }
 }
 
 pub struct SeqListSet<T> {
@@ -51,25 +81,20 @@ impl<T> SeqListSet<T> {
 
 impl<T: Hashable> Set<T> for SeqListSet<T> {
     fn contains(&self, element: T) -> bool {
-        Node::find(&self.head, element.hash()).1
+        let (_node, present) = Node::find(&self.head, element.hash());
+        present
     }
 }
 
 impl<T: Hashable> MutSet<T> for SeqListSet<T> {
     fn add(&mut self, element: T) -> bool {
         let (node, present) = Node::find_mut(&mut self.head, element.hash());
-        if !present {
-            let new_node = Node { item: element, next: node.take() };
-            *node = Some(Box::new(new_node));
-        }
+        if !present { Node::insert(node, element); }
         !present
     }
     fn remove(&mut self, element: T) -> bool {
         let (node, present) = Node::find_mut(&mut self.head, element.hash());
-        if present {
-            // if find_mut().1 is true, then find_mut().0 should be Some
-            *node = node.take().expect("find_mut violated contract").next;
-        }
+        if present { assert!(Node::remove(node).is_ok()); }
         present
     }
 }
